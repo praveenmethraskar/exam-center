7c190d57cd9f1dc3ef230aeaac152d6d98dac845
Praveen Methraskar <55995179+praveenmethraskar@users.noreply.github.com>
2025-01-23 14:28:49 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
     setErrorMessage("")
 
     try {
-      const response = await axios.post("http://localhost:3001/api/login", {
+      const response = await axios.post("http://0.0.0.0:3001/api/login", {
         username,
         password,
       })
